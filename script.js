1529f598ab3f9b388d4e65044bc83a68f6b48774
Matthew Smith <mateoelcule@gmail.com>
2018-11-24 15:56:08 -0800

    Wrote 'getLikes' function and populated infowindows with number of user likes using the FourSquare API.

--- script.js
+++ script.js
@@ -4,14 +4,27 @@ function initMap() {
     zoom: 12
   });
 
-  fetchedLocations = fetchLocations();
-  fetchedLocations.then(function(locationData) {
+  let fetchedLocations = fetchLocations();
+  fetchedLocations
+  .then(function(locationData) {
     setMarkers(locationData, map);
+  })
+  .catch(function(error) {
+    console.log(error);
   });
+
+  // let likes = getLikes(locationID);
+  // likes.then(function(response) {
+  //   console.log(response);
+  //   return response;
+  // });
+
 }
 
 function fetchLocations() {
-   return fetch('https://api.foursquare.com/v2/venues/explore?client_id=JEASTQIYAOQHC5EJ45NM4QUSD2AS11EADPF51VDM42O4Q13A&client_secret=GEMFOAQ5IBMRS1ROLEFMRMNEZSV0R3QYPZEMLALQJNUFANCH&v=20180323&limit=15&ll=40.7243,-74.0018&near=Agoura HIlls, CA')
+   return fetch(
+    'https://api.foursquare.com/v2/venues/explore?client_id=JEASTQIYAOQHC5EJ45NM4QUSD2AS11EADPF51VDM42O4Q13A&client_secret=GEMFOAQ5IBMRS1ROLEFMRMNEZSV0R3QYPZEMLALQJNUFANCH&v=20180323&limit=15&ll=40.7243,-74.0018&near=Agoura Hills, CA'
+    )
         .then(function(response) {
             return response.json();
         })
@@ -19,13 +32,62 @@ function fetchLocations() {
           let locations = populateLocationsArray(data);
           return locations;
         })
-        .catch(function(e) {
-            console.log(e);
+        .catch(function(error) {
+            console.log(error);
         });
 }
 
+function getLikes(venueID) {
+  return fetch(
+    'https://api.foursquare.com/v2/venues/' + venueID + '/likes?client_id=JEASTQIYAOQHC5EJ45NM4QUSD2AS11EADPF51VDM42O4Q13A&client_secret=GEMFOAQ5IBMRS1ROLEFMRMNEZSV0R3QYPZEMLALQJNUFANCH&v=20180323'
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        let likes = data.response.likes.summary;
+        return likes;
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+
+}
+
+function populateLocationsArray(locationsData) {
+  let responseLength = locationsData.response.groups[0].items.length;
+  let locations = [];
+  for (let i = 0; i < responseLength; i++) {
+    let location = locationsData.response.groups[0].items[i].venue;
+    locations.push(location);  }
+  return locations;
+}
+
+function setMarkers (locationData, map) {
+    let responseLength = locationData.length;
+    let markers = [];
+    for (let i = 0; i < responseLength; i++) {
+      let lat = locationData[i].location.lat;
+      let lng = locationData[i].location.lng;
+      let position = {lat: lat, lng: lng};
+      let title = locationData[i].name;
+      let marker = new google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        id: i
+      });
+
+      markers.push(marker);
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfoWindow, locationData[i]);
+      });
+    }
+  let largeInfoWindow = new google.maps.InfoWindow();
+}
+
 function populateInfoWindow(marker, infowindow, locationData) {
-  console.log(locationData);
   if (infowindow.marker != marker) {
     infowindow.setContent('');
     infowindow.marker = marker;
@@ -34,6 +96,12 @@ function populateInfoWindow(marker, infowindow, locationData) {
     });
     let streetViewService = new google.maps.StreetViewService();
     let radius = 50;
+    let locationID = locationData.id;
+    let likes = getLikes(locationID);
+    likes.then(function(response) {
+      document.querySelector('.likes').innerHTML = response + ' by FourSquare users';
+    })
+
     function getStreetView(data, status) {
       let infowindowContent = `
           <h3 class="markerTitle">${marker.title}</h3>
@@ -41,6 +109,7 @@ function populateInfoWindow(marker, infowindow, locationData) {
           <div class="details">
             <div>${locationData.location.formattedAddress[0]}</div>
             <div>${locationData.location.formattedAddress[1]}</div>
+            <div class="likes"></div>
           </div>
           `;
       let noStreetViewMessage = '<div class ="details">No street view found.</div>';
@@ -64,43 +133,6 @@ function populateInfoWindow(marker, infowindow, locationData) {
     infowindow.open(map, marker);
   }
 }
-
-function populateLocationsArray(locationsData) {
-  let responseLength = locationsData.response.groups[0].items.length;
-  let locations = [];
-  for (let i = 0; i < responseLength; i++) {
-    let location = locationsData.response.groups[0].items[i].venue;
-    locations.push(location);
-  }
-  return locations;
-}
-
-function setMarkers (locationData, map) {
-    console.log(locationData);
-    let responseLength = locationData.length;
-    let markers = [];
-    for (let i = 0; i < responseLength; i++) {
-      let lat = locationData[i].location.lat;
-      let long = locationData[i].location.lng;
-      let position = {lat: lat, lng: long};
-      let title = locationData[i].name;
-      let marker = new google.maps.Marker({
-        map: map,
-        position: position,
-        title: title,
-        animation: google.maps.Animation.DROP,
-        id: i
-      });
-
-      markers.push(marker);
-      marker.addListener('click', function() {
-        populateInfoWindow(this, largeInfoWindow, locationData[i]);
-      });
-    }
-  let largeInfoWindow = new google.maps.InfoWindow();
-}
-
-
 
 
 
