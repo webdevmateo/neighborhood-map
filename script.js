80129f5fd93d92aef88b8048d73510e94a91e4cd
Matthew Smith <mateoelcule@gmail.com>
2018-11-23 20:01:00 -0800

    Refactored 'script.js' so that locations load on the map from the FourSquare API.

--- script.js
+++ script.js
@@ -1,43 +1,31 @@
-
 function initMap() {
-  let markers = [];
   const map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 34.15334, lng: -118.761676},
     zoom: 12
   });
 
-  fetchLocations();
-
-  let locations = [
-    {title: 'Sumac Elementary School', location: {lat: 34.161085, lng: -118.755882}},
-    {title: 'Costco', location: {lat: 34.151917, lng: -118.797079}},
-    {title: 'Target', location: {lat: 34.147689, lng: -118.793616}},
-    {title: 'Trader Joe\'s', location: {lat: 34.146443, lng: -118.756649}},
-    {title: 'Yarrow Family YMCA', location: {lat: 34.158963, lng: -118.800003}},
-    {title: 'Oak Canyon Splash Pad', location: {lat: 34.184542, lng: -118.770962}}
-  ]
-
-  let largeInfoWindow = new google.maps.InfoWindow();
-
-  for (let i = 0; i < locations.length; i++) {
-    let position = locations[i].location;
-    let title = locations[i].title;
-    let marker = new google.maps.Marker({
-      map: map,
-      position: position,
-      title: title,
-      animation: google.maps.Animation.DROP,
-      id: i
-    });
-
-    markers.push(marker);
-    marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfoWindow);
-    });
-  }
+  fetchedLocations = fetchLocations();
+  fetchedLocations.then(function(locationData) {
+    setMarkers(locationData, map);
+  });
 }
 
-function populateInfoWindow(marker, infowindow) {
+function fetchLocations() {
+   return fetch('https://api.foursquare.com/v2/venues/explore?client_id=JEASTQIYAOQHC5EJ45NM4QUSD2AS11EADPF51VDM42O4Q13A&client_secret=GEMFOAQ5IBMRS1ROLEFMRMNEZSV0R3QYPZEMLALQJNUFANCH&v=20180323&limit=15&ll=40.7243,-74.0018&near=Agoura HIlls, CA')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+          let locations = populateLocationsArray(data);
+          return locations;
+        })
+        .catch(function(e) {
+            console.log(e);
+        });
+}
+
+function populateInfoWindow(marker, infowindow, locationData) {
+  console.log(locationData);
   if (infowindow.marker != marker) {
     infowindow.setContent('');
     infowindow.marker = marker;
@@ -47,12 +35,19 @@ function populateInfoWindow(marker, infowindow) {
     let streetViewService = new google.maps.StreetViewService();
     let radius = 50;
     function getStreetView(data, status) {
+      let infowindowContent = `
+          <h3 class="markerTitle">${marker.title}</h3>
+          <div id="pano"></div>
+          <div class="details">
+            <div>${locationData.location.formattedAddress[0]}</div>
+            <div>${locationData.location.formattedAddress[1]}</div>
+          </div>
+          `;
+      let noStreetViewMessage = '<div class ="details">No street view found.</div>';
       if (status == google.maps.StreetViewStatus.OK) {
-        console.log(data);
-        console.log(status);
         let nearStreetViewLocation = data.location.latLng;
         let heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
-        infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+        infowindow.setContent(infowindowContent);
         let panoramaOptions = {
           position: nearStreetViewLocation,
           pov: {
@@ -62,26 +57,12 @@ function populateInfoWindow(marker, infowindow) {
         }
         let panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
       } else {
-        infowindow.setContent('<div>' + marker.title + '</div>' +
-                '<div>No Street View Found</div>');
+        infowindow.setContent(noStreetViewMessage + infowindowContent);
       }
     }
     streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
     infowindow.open(map, marker);
   }
-}
-
-function fetchLocations() {
-  return fetch('https://api.foursquare.com/v2/venues/explore?client_id=JEASTQIYAOQHC5EJ45NM4QUSD2AS11EADPF51VDM42O4Q13A&client_secret=GEMFOAQ5IBMRS1ROLEFMRMNEZSV0R3QYPZEMLALQJNUFANCH&v=20180323&limit=20&ll=40.7243,-74.0018&near=Agoura HIlls, CA')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-      populateLocationsArray(data);
-    })
-    .catch(function(e) {
-        console.log(e);
-    });
 }
 
 function populateLocationsArray(locationsData) {
@@ -91,16 +72,32 @@ function populateLocationsArray(locationsData) {
     let location = locationsData.response.groups[0].items[i].venue;
     locations.push(location);
   }
+  return locations;
+}
 
-  console.log(locations);
-  // let locations = [
-  //   {title: 'Sumac Elementary School', location: {lat: 34.161085, lng: -118.755882}},
-  //   {title: 'Costco', location: {lat: 34.151917, lng: -118.797079}},
-  //   {title: 'Target', location: {lat: 34.147689, lng: -118.793616}},
-  //   {title: 'Trader Joe\'s', location: {lat: 34.146443, lng: -118.756649}},
-  //   {title: 'Yarrow Family YMCA', location: {lat: 34.158963, lng: -118.800003}},
-  //   {title: 'Oak Canyon Splash Pad', location: {lat: 34.184542, lng: -118.770962}}
-  // ]
+function setMarkers (locationData, map) {
+    console.log(locationData);
+    let responseLength = locationData.length;
+    let markers = [];
+    for (let i = 0; i < responseLength; i++) {
+      let lat = locationData[i].location.lat;
+      let long = locationData[i].location.lng;
+      let position = {lat: lat, lng: long};
+      let title = locationData[i].name;
+      let marker = new google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        id: i
+      });
+
+      markers.push(marker);
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfoWindow, locationData[i]);
+      });
+    }
+  let largeInfoWindow = new google.maps.InfoWindow();
 }
 
 
